6e74955e3efcdfeb99c2a7e93ae004034cb57f86
abubakaraliyu09 <45861855+abubakaraliyu09@users.noreply.github.com>
2022-02-02 14:31:24 +0000

    Add files via upload

---
+++ .npy to csv.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import glob
+import numpy as np
+import os
+
+ids=[]
+features=[]
+
+for f in glob.glob('data1\\*.npy'):
+    names=os.path.basename(f)
+    name=names.strip('_AUDIO_audio_features.npy')
+    ids.append(name)
+    #print(name)
+    features.append(np.load(f))
+x=pd.DataFrame(np.concatenate(features))#.to_csv('Features.csv',index=False)
+y=pd.DataFrame(ids)#.to_csv('IDS.csv',index=False)
+horizontal_stack = pd.concat([y, x], axis=1)
+print(horizontal_stack)
+horizontal_stack.to_csv('Concat.csv',index=False)
+
+
+
+#df = pd.DataFrame.from_dict(dic, orient='columns')
+#print(df)
+#df.to_csv(f'{output_path}/{dataset_prefix}_{i}_knn.csv', header=True, index=False)
